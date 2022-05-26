c4aa781e2d1bef811666ae4d5bc0f19b8108fac5
huntmori <huntmori04x22@gmail.com>
2022-05-27 00:45:59 +0900

    adjust dbconfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
+import { MariaDBConfigService } from './config/database/mariadb.config.service';
+import { MariaDbConfigModule } from './config/database/mariadb.config.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.' + process.env.NODE_ENV + '.env',
+      ignoreEnvFile: false,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [MariaDbConfigModule],
+      useClass: MariaDBConfigService,
+      inject: [MariaDBConfigService],
+    }),
+  ],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
